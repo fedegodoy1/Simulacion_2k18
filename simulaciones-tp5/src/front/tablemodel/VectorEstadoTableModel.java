803d5877376056fcac725daeaf09b839c6125430
Nicolas Hefty <nicolas.hefty@aa.com>
2018-05-21 00:11:22 -0300

    Agregando algunas cosas de columnas

--- simulaciones-tp5/src/front/tablemodel/VectorEstadoTableModel.java
+++ simulaciones-tp5/src/front/tablemodel/VectorEstadoTableModel.java
@@ -76,6 +76,38 @@ public final class VectorEstadoTableModel extends DefaultTableModel implements V
         
         //Agregar cada objeto de columna a la lista
 
+        //https://coderanch.com/t/333340/java/set-Line-wrap-Column-JTable 
+        columns.add(new Columna("Evento", String.class, 100));
         columns.add(new Columna("Reloj", LocalTime.class, 75));
+        columns.add(new Columna("LLegada Alumnos: RND", LocalTime.class, 75));
+        columns.add(new Columna("Llegada Alumnos: Tiempo llegada", LocalTime.class, 75));
+        columns.add(new Columna("Llegada Alumnos: Proxima llegada", LocalTime.class, 75));
+        columns.add(new Columna("Llegada Mantenimiento: RND", LocalTime.class, 75));
+        columns.add(new Columna("Llegada Mantenimiento: Tiempo llegada", LocalTime.class, 75));
+        columns.add(new Columna("Llegada Mantenimiento: Proxima llegada", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: RND", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: Tiempo inscripcion", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: Fin inscripcion equipo 1", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: Fin inscripcion equipo 2", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: Fin inscripcion equipo 3", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: Fin inscripcion equipo 4", LocalTime.class, 75));
+        columns.add(new Columna("Fin Inscripcion: Fin inscripcion equipo 5", LocalTime.class, 75));
+        columns.add(new Columna("Fin Mantenimiento: RND", LocalTime.class, 75));
+        columns.add(new Columna("Fin Mantenimiento: Tiempo mantenimiento", LocalTime.class, 75));
+        columns.add(new Columna("Fin Mantenimiento: Hora fin", LocalTime.class, 75));
+        columns.add(new Columna("Cola Alumnos", LocalTime.class, 75));
+        columns.add(new Columna("Cola Mantenimiento", LocalTime.class, 75));
+        columns.add(new Columna("Maquina 1: Estado", LocalTime.class, 75));
+        //Resto de las máquinas
+        columns.add(new Columna("Acum. Inscripciones", LocalTime.class, 75));
+        columns.add(new Columna("Acum. Alumnos que llegan", LocalTime.class, 75));
+        columns.add(new Columna("Acum. Alumnos que llegan y se van", LocalTime.class, 75));
+        columns.add(new Columna("Alumnos", LocalTime.class, 75));
+        columns.add(new Columna("Mantenimiento", LocalTime.class, 75));
+    }
+    
+    public List<Columna> getColumnaList()
+    {
+        return columns;
     }
 }
