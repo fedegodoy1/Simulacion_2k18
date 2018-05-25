20e1e0fa9f973be46fdb936b1ae665e1d788300e
Nicolas Hefty <nicolas.hefty@aa.com>
2018-05-25 17:53:49 -0300

    Muevo Distribuciones a que use todo double

--- simulaciones-tp5/src/objects/Distribuciones.java
+++ simulaciones-tp5/src/objects/Distribuciones.java
@@ -3,6 +3,7 @@ package objects;
 
 import static java.lang.Math.PI;
 import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 import static java.lang.Math.log;
 import static java.lang.Math.sqrt;
 import java.util.Random;
@@ -12,21 +13,24 @@ public class Distribuciones {
     
     private static Random rnd1 = new Random();
     private static Random rnd2 = new Random();
+    private static int var = 0;
     
-    public static float calcular_uniforme(float a, float b) {
-        return a + (rnd1.nextFloat() * (b-a));
+    public static double calcular_uniforme(double a, double b) {
+        return a + (rnd1.nextDouble()* (b-a));
     }
              
-    public static float calcular_normal(float media, float desviacion) {
+    public static double calcular_normal(double media, double desviacion) {
         
-        double raiz = sqrt((-2)* log(rnd1.nextFloat()));
-        double coseno = cos(2*PI*rnd2.nextFloat());
-        return (float) ((raiz * coseno) * desviacion + media);
+        double raiz = sqrt((-2)* log(rnd1.nextDouble()));
+        
+        double segundo_miembro = var % 2 == 0 ? cos(2*PI*rnd2.nextDouble()) : sin(2*PI*rnd2.nextDouble());
+        var++;
+        return ((raiz * segundo_miembro) * desviacion + media);
         
     }
     
-    public static float calcular_exponencial(float media) {
-        return (media*-1) * (float)Math.log(1-rnd1.nextFloat());
+    public static double calcular_exponencial(double media) {
+        return (media*-1) * Math.log(1-rnd1.nextDouble());
     }
    
 }
