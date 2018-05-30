00209c0bd2e93b6868ff30ef74bb1044045da951
Nicolas Hefty <nicolas.hefty@aa.com>
2018-05-29 23:03:23 -0300

    Evento Regreso Alumno

--- simulaciones-tp5/src/control/eventos/EventoRegresoAlumno.java
+++ simulaciones-tp5/src/control/eventos/EventoRegresoAlumno.java
@@ -7,6 +7,9 @@ package control.eventos;
 
 import control.ControladorSimulacion;
 import control.VectorEstado;
+import java.util.ArrayList;
+import objects.Alumno;
+import objects.Maquina;
 
 /**
  *
@@ -19,9 +22,72 @@ public class EventoRegresoAlumno extends Evento
         VectorEstado actual = ControladorSimulacion.getVectorActual();
         VectorEstado anterior = ControladorSimulacion.getVectorAnterior();
         
+        actual.setAcumuladoAlumnosQueLlegan(anterior.getAcumuladoAlumnosQueLlegan());
+        actual.setAcumuladoAlumnosQueLleganYSeVan(anterior.getAcumuladoAlumnosQueLleganYSeVan());
+        actual.setAcumuladoInscripciones(anterior.getAcumuladoInscripciones());
+        actual.setAlumnos(new ArrayList<>(anterior.getAlumnos()));
+        actual.setColaAlumnos(anterior.getColaAlumnos().clone());
+        actual.setEncargado(anterior.getEncargado().clone());
+        actual.setFinInscripcion(anterior.getFinInscripcion().clone());
+        actual.setFinMantenimiento(anterior.getFinMantenimiento().clone());
+        actual.setInicioMantenimiento(anterior.getInicioMantenimiento().clone());
+        actual.setLlegadaAlumno(anterior.getLlegadaAlumno().clone());
+        actual.setMaquinas(new ArrayList<>(anterior.getMaquinasList()));
+        
+        Alumno alumnoQueVuelve = encontrarAlumnoQueVuelve(actual);
+        
+        if (actual.getColaAlumnos().getColaAlumnos() > 4)
+        {
+            //Se va de nuevo el...
+            alumnoQueVuelve.setEstado(Alumno.Estado.ESPERANDO_PARA_REGRESAR);
+            alumnoQueVuelve.setHora_regreso_sistema(actual.getReloj() + 30);
+        }
+        else if (actual.getColaAlumnos().getColaAlumnos() > 0)
+        {
+            //A la cola
+            alumnoQueVuelve.setEstado(Alumno.Estado.ESPERANDO_MAQUINA);
+            alumnoQueVuelve.setHora_regreso_sistema(Double.MAX_VALUE);
+            
+            actual.getColaAlumnos().agregarAlumnoCola();
+        }
+        else
+        {
+            //Vemos si hay maquina libre
+            boolean hayMaquinaLibre = false;
+            Maquina maquinaLibre = null;
+            
+            for (Maquina maq : actual.getMaquinasList())
+            {
+                if (maq.getEstado().equals(Maquina.Estado.LIBRE))
+                {
+                    hayMaquinaLibre = true;
+                    maquinaLibre = maq;
+                    break;
+                }
+            }
+            
+            if (hayMaquinaLibre)
+            {
+                alumnoQueVuelve.setEstado(Alumno.Estado.INSCRIBIENDOSE);
+                alumnoQueVuelve.setHora_regreso_sistema(Double.MAX_VALUE);
+                alumnoQueVuelve.setMaquinaInscripcion(maquinaLibre.getId());
+            }
+            else
+            {
+                alumnoQueVuelve.setEstado(Alumno.Estado.ESPERANDO_MAQUINA);
+                alumnoQueVuelve.setHora_regreso_sistema(Double.MAX_VALUE);
+                
+                actual.getColaAlumnos().agregarAlumnoCola();
+            }
+        }
+        
         /**
          * Borrar atributo de regreso del alumno.
          * Hacer lo mismo q en LlegadaAlumno pero sin incrementar ACs
          */
+    }
+
+    private Alumno encontrarAlumnoQueVuelve(VectorEstado actual) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
